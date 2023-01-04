8e6d3a9b3ace7cc41fa7c73fde6184060b446bd1
Sameer <69124381+SAMEER-40@users.noreply.github.com>
2023-01-04 11:46:59 +0530

    Store and Retrieve Data

--- src/B/urls.py
+++ src/B/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for B project.
+URL configuration for cfehome project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +15,44 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from auth import views as auth_views
+from checkouts import views as checkout_views
+from landing import views as landing_views
+from subscriptions import views as subscriptions_views
+from .views import (
+    home_view, 
+    about_view, 
+    pw_protected_view,
+    user_only_view,
+    staff_only_view
+)
 
-from .views import home_page_view
 urlpatterns = [
-    path("hello-world/", home_page_view),
-    path('admin/', admin.site.urls),
+    path("", landing_views.landing_dashboard_page_view, name='home'),
+    path("checkout/sub-price/<int:price_id>/", 
+            checkout_views.product_price_redirect_view,
+            name='sub-price-checkout'
+            ),
+    path("checkout/start/", 
+            checkout_views.checkout_redirect_view,
+            name='stripe-checkout-start'
+            ),
+    path("checkout/success/", 
+            checkout_views.checkout_finalize_view,
+            name='stripe-checkout-end'
+            ),
+    path("pricing/", subscriptions_views.subscription_price_view, name='pricing'),
+    path("pricing/<str:interval>/", subscriptions_views.subscription_price_view, name='pricing_interval'),
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("hello-world.html", home_view),
+    path('accounts/billing/', subscriptions_views.user_subscription_view, name='user_subscription'),
+    path('accounts/billing/cancel', subscriptions_views.user_subscription_cancel_view, name='user_subscription_cancel'),
+    path('accounts/', include('allauth.urls')),
+    path('protected/user-only/', user_only_view),
+    path('protected/staff-only/', staff_only_view),
+    path('protected/', pw_protected_view),
+    path('profiles/', include('profiles.urls')),
+    path("admin/", admin.site.urls),
 ]
