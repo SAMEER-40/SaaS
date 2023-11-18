4de85c23f57e8a90fd8e735321c9ce0b43d1559f
Sameer <69124381+SAMEER-40@users.noreply.github.com>
2023-11-18 11:47:59 +0530

    djano debug

--- src/B/settings.py
+++ src/B/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +23,8 @@ print('BASE_DIR', BASE_DIR)
 SECRET_KEY = 'django-insecure-4=_kk0rdpw%&+^h^=w)b8f^zg*zds)kmb4ggp3a^yx7y8oovfl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower == "true"
+print('DEBUG', DEBUG)
 ALLOWED_HOSTS = [
 
     ".railway.app"
